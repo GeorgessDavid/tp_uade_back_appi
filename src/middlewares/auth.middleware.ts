c25c8f0afc6a7e1a288cf6f777dd05b69bd52fa5
Georges David <georgessdavid@gmail.com>
2025-11-27 16:40:07 -0300

    Añadido: Middleware de autenticación.

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomError } from '../error/custom.error';
+
+const JWT_SECRET: string = process.env.JWT_SECRET!;
+
+/**
+ * Middleware para autenticar solicitudes usando JWT.
+ * Verifica el token guardado en las cookies de session.
+ * Si no es válido, responde con un error 401.
+ * @param {Request} req -- Objeto de solicitud de Express.
+ * @param {Response} res -- Objeto de respuesta de Express.
+ * @param {NextFunction} next -- Función para pasar al siguiente middleware.
+ */
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+    const { token } = req.session.auth || {};
+
+    if (!token) throw new CustomError(401, 'Token de autenticación no proporcionado');
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        
+        if(!decoded) throw new CustomError(401, 'Token de autenticación inválido');
+        next();
+
+    } catch (error) {
+        if (error instanceof CustomError) res.status(error.status).json({ message: error.message });
+        else res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};  
